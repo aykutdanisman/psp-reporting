8ddb03059cd41e3a32dbabc1c4d0e80f0fc5e613
aykutdanisman <aykutdanisman@gmail.com>
2019-05-08 07:23:09 +0100

    initial commit with dependencies : lombok, jpa, postgreSQL, Security,web, rest repositories

---
+++ psp-reporting/src/main/java/com/kutztech/reporting/demo/PspReportingApplication.java
@@ -0,0 +1,13 @@
+package com.kutztech.reporting.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PspReportingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PspReportingApplication.class, args);
+	}
+
+}
